5736ec9e95317d5b5b42f1d37443f3c11ed2d2b9
Nabil Adem <nabiladam7@gmail.com>
2025-10-10 23:59:45 -0500

    Implement more optimal image compression with binary and linear search

--- internal/compressor/compressor.go
+++ internal/compressor/compressor.go
@@ -16,8 +16,7 @@ import (
 /* inputPath (string) - path of the input image; outputPath (string) - path of the input image
 // maxSize (int) - maximum size of the image in byes; outputFormat (string) - jpeg, png, or gif */
 func CompressImage(inputPath string, outputPath string, maxSize int, outputFormat string, quality int, verbose bool) error {
-
-    const minWidth = 100;
+    const MinWidth = 100;
 
     if verbose {
         fmt.Println("Starting compression...")
@@ -43,22 +42,26 @@ func CompressImage(inputPath string, outputPath string, maxSize int, outputForma
         return fmt.Errorf("failed to decode image: %v", err)
     }
 
-    bounds := img.Bounds()
-    width := bounds.Dx()
+    width := img.Bounds().Dx()
+    best := 0
+    var buf *bytes.Buffer
+    
+    low, high := MinWidth, width
+    for low <= high {
+        mid := (low + high) / 2
 
-    for width > minWidth {
         // resize to fit width while maintaining aspect ratio
-        resizedImg := resize.Resize(uint(width), 0, img, resize.Lanczos3)
+        resizedImg := resize.Resize(uint(mid), 0, img, resize.Lanczos3)
 
         // encode the resized image to the desired format
-        var buf bytes.Buffer
+        var binaryBuf bytes.Buffer
         switch outputFormat {
         case "jpeg":
-            err = jpeg.Encode(&buf, resizedImg, &jpeg.Options{Quality: quality})
+            err = jpeg.Encode(&binaryBuf, resizedImg, &jpeg.Options{Quality: quality})
         case "png":
-            err = png.Encode(&buf, resizedImg)
+            err = png.Encode(&binaryBuf, resizedImg)
         case "gif":
-            err = gif.Encode(&buf, resizedImg, nil)
+            err = gif.Encode(&binaryBuf, resizedImg, nil)
         default:
             return fmt.Errorf("unsupported file format: %v. Supported formats are: jpeg, png, gif", outputFormat)
         }
@@ -67,34 +70,81 @@ func CompressImage(inputPath string, outputPath string, maxSize int, outputForma
             return fmt.Errorf("failed to encode image: %v", err)
         }
 
+        size := binaryBuf.Len()
+
         if verbose {
-            fmt.Printf("Trying width: %d -> Compressed size: %.2f bytes\n", width, float64(buf.Len()))
+            fmt.Printf("[binary] Trying width: %d -> Compressed size: %.2f KB\n", width, float64(size) / 1024.0)
         }
-
-        if buf.Len() <= maxSize {
-            // create the output file
-            outFile, err := os.Create(outputPath)
-
-            if err != nil {
-                return fmt.Errorf("failed to create output file: %v", err)
-            }
-
-            defer outFile.Close()
-
-            if verbose {
-                fmt.Println("Compressing and saving image...")
-            }
-
-            _, err = buf.WriteTo(outFile)
-            if err != nil {
-                return fmt.Errorf("failed to write compressed image to file: %v", err)
-            }
-
-            return nil
+        
+        if size > maxSize {
+            high = mid - 1
+        } else {
+            best = mid
+            buf = &binaryBuf
+            low = mid + 1
         }
-
-        width = width * 50 / 100 // reduce width by 50%
+    }
+    
+    if best == 0 {
+        return fmt.Errorf("cannot compress image to the desired size of %d bytes", maxSize)
     }
 
-    return fmt.Errorf("failed to compress image under %d bytes", maxSize)
+    step := best / 20
+    for width = best; width >= MinWidth; width -= step {
+        // resize to fit width while maintaining aspect ratio
+        resizedImg := resize.Resize(uint(width), 0, img, resize.Lanczos3)
+
+        // encode the resized image to the desired format
+        var binaryBuf bytes.Buffer
+        switch outputFormat {
+        case "jpeg":
+            err = jpeg.Encode(&binaryBuf, resizedImg, &jpeg.Options{Quality: quality})
+        case "png":
+            err = png.Encode(&binaryBuf, resizedImg)
+        case "gif":
+            err = gif.Encode(&binaryBuf, resizedImg, nil)
+        default:
+            return fmt.Errorf("unsupported file format: %v. Supported formats are: jpeg, png, gif", outputFormat)
+        }
+
+        if err != nil {
+            return fmt.Errorf("failed to encode image: %v", err)
+        }
+
+        size := binaryBuf.Len()
+
+        if verbose {
+            fmt.Printf("[linear] Trying width: %d -> Compressed size: %.2f KB\n", width, float64(size) / 1024.0)
+        }
+        
+        if size <= maxSize {
+            buf = &binaryBuf
+            break
+        }
+    }
+
+    if buf == nil {
+        return fmt.Errorf("cannot compress image to the desired size of %d bytes", maxSize)
+    }
+
+    // create the output file
+    outFile, err := os.Create(outputPath)
+
+    if err != nil {
+        return fmt.Errorf("failed to create output file: %v", err)
+    }
+
+    defer outFile.Close()
+
+    if verbose {
+        fmt.Println("Compressing and saving image...")
+    }
+
+    _, err = buf.WriteTo(outFile)
+
+    if err != nil {
+        return fmt.Errorf("failed to write compressed image to file: %v", err)
+    }
+
+    return nil
 }
